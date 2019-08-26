debc9ad08a8e7110b38236a5761750c69faeecbb
lukemenard <lukemenard@gmail.com>
2019-08-26 14:19:58 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskList = new TaskList();
+
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+
+  const newTaskList = document.getElementById("tasks");
+
+  const renderApp = () => (newTaskList.innerHTML = taskList.renderTasks())
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(taskDescription.value);
+    e.target.reset();
+    renderApp();
+  });
+
+  newTaskList.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
