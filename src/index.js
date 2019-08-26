0b6a27c66421faa5ab2f5b40c15d416ee4a64ad7
jollyjerr <jollyjerr@gmail.com>
2019-08-26 15:30:00 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const newTaskForm = document.querySelector('#create-task-form');
+    // const newTaskDescription = document.querySelector('#new-task-description');
+    // const newTaskPriority = document.querySelector('#new-task-priority');
+
+    // const taskList = document.querySelector('#tasks');
+
+    newTaskForm.addEventListener("submit", createNewTask);
 });
+
+function createNewTask(event) {
+    event.preventDefault();
+
+    const newTaskDescription = document.querySelector('#new-task-description');
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+
+    document.getElementById("tasks").appendChild(newTask);
+    event.target.reset();
+}
